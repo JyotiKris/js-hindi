336c63b8cf6d458ad1561f11cc43883414d2a553
JyotiKris <87688364+JyotiKris@users.noreply.github.com>
2023-09-25 16:45:45 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer verson
+// alert( 3+3)  we are using node js not browser
+console.log(3+3)  //code should be readable// 
+console.log("jyoti");
+
+let name="jyoti"
+let age= 23
+let isLoggedIn = false
+
+/* Types of Numeric Datatype:
+number= 2 to the power of 53
+bigint 
+string = ""
+boolean = true/false
+nul = stad alone value (representation of empty value)
+undefined = unassigned value
+symbol = to show uniqueness
+*/
+
+// object
+
+console.log(typeof "jyoti");//string
+console.log(typeof age);//number
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
